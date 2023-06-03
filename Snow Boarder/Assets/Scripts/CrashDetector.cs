57eb0310cb5d8d376617843c824fd2580f0683be
Miguel Angel Briseño Flores <miiguelb07@gmail.com>
2023-06-03 03:11:34 -0600

    added scenemanager feature

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Terrain"){
             Debug.Log("Ouch!");
+            SceneManager.LoadScene(0);
         }
     }
 }
